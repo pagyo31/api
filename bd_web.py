c1723e2ca7bc33a39bfa344f4f97b12dc2fcc242
pagyo31 <54297417+pagyo31@users.noreply.github.com>
2020-06-24 20:36:58 -0300

    Add files via upload

---
+++ bd_web.py
@@ -0,0 +1,42 @@
+import pymysql
+from flask import Flask, jsonify, request
+
+
+conexao = pymysql.connect( #conectando ao banco de dados
+    host= 'localhost',
+    user= 'root',
+    passwd= '',
+    database= 'python'
+)
+
+app = Flask(__name__)
+
+clientes = []
+
+cursor = conexao.cursor() #cria um cursor para navegr pelo banco de dados
+cursor.execute("SELECT * FROM clientes")
+for c in cursor:
+    clientes.append(c)
+
+print(clientes)
+
+@app.route('/',)
+def home():
+    return jsonify(clientes), 200
+
+@app.route('/', methods=['POST'])
+def post():
+    data = request.get_json()
+    user = data["user"]
+    passw = data["pass"]
+    add_sql = "INSERT INTO clientes(user,pass) VALUES (%s,%s)" #adicionadno o dado mesmo na tabela
+    valor = (user, passw) #valor q serao acrescentados na tabela
+    cursor.execute(add_sql,valor)
+    conexao.commit() #para realmente efetivar qualquer troca na tabela
+    print(cursor.rowcount,"inserida com sucesso")
+    clientes.append(data)
+    return jsonify(clientes), 201
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
